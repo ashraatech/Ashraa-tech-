337a440ee7d83e56368b12c6d37585bd6b723679
Ayush Mishra <mishraayu2171@gmail.com>
2026-05-24 09:25:06 +0530

    ifelse

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+const age =32
+if (age<18){
+    console.log("not approved")
+}else if (age>=18 && age<=60){
+    console.log("approved")
+}else if (age>60 && age<=150){
+    console.log("not approved")
+}
+else{
+    console.log("death")
+}
+        
