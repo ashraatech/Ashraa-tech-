1c0454b12084ef6b912b529ff797b94f980e598b
Ayush Mishra <mishraayu2171@gmail.com>
2026-05-24 09:36:31 +0530

    loop

--- ifelse.js
+++ ifelse.js
@@ -1,12 +1,10 @@
-const age =32
-if (age<18){
-    console.log("not approved")
-}else if (age>=18 && age<=60){
-    console.log("approved")
-}else if (age>60 && age<=150){
-    console.log("not approved")
+const age = 32;
+if (age < 18) {
+  console.log("not approved");
+} else if (age >= 18 && age <= 60) {
+  console.log("approved");
+} else if (age > 60 && age <= 150) {
+  console.log("not approved");
+} else {
+  console.log("death");
 }
-else{
-    console.log("death")
-}
-        
